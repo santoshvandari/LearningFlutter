1a84f42f3017cb63b674f89d0e66495fd361f7a3
Santosh Bhandari <santoshvandari1@gmail.com>
2024-02-19 04:34:18 +0545

    Initial

--- Practice/myfirstapp/lib/main.dart
+++ Practice/myfirstapp/lib/main.dart
@@ -28,7 +28,8 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme:
+            ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 6, 25, 40)),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Testing my First Application'),
@@ -118,7 +119,7 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: const Icon(Icons.difference),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
