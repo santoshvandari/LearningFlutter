ce13a39c80c80ada988b21757a5e34b2fdff0ffe
Santosh Bhandari <santoshvandari1@gmail.com>
2024-09-20 20:05:23 +0545

    Inital

--- currencyconverter/lib/main.dart
+++ currencyconverter/lib/main.dart
@@ -1,1 +1,20 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      body: Text(
+        "Santosh Bhandari",
+        style: TextStyle(fontSize: 16, color: Color(222222)),
+      ),
+    ));
+  }
+}
